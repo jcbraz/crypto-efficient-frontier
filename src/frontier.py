f71c9aacf03ffbe017364e9e66592a168142786b
jcbraz <josecbraz@outlook.com>
2023-02-19 00:03:13 +0000

    Get mean and covariance data

--- src/frontier.py
+++ src/frontier.py
@@ -4,9 +4,11 @@ import os
 
 
 def getData(assets: list, interval: str, start: str, end: str):
+
     client = Client(os.environ.get("BINANCE_API_KEY"),
                     os.environ.get("BINANCE_API_SECRET"))
     frames = {}
+    returns = []
 
     for asset in assets:
         data = client.get_historical_klines(asset, interval, start, end)
@@ -14,13 +16,19 @@ def getData(assets: list, interval: str, start: str, end: str):
                           'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
 
         df.drop(columns=['open_time', 'close_time', 'quote_asset_volume', 'number_of_trades',
-                'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'], axis=1, inplace=True)
+                'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'], inplace=True)
 
         df = df.apply(pd.to_numeric)
-        df['close_pct_change'] = df['close'].pct_change()
-        frames[asset] = df
+        df['pct_change'] = df['close'].pct_change()
+        meanValues = df['pct_change'].mean()
+        covariance = df.cov()
+        returns.append(meanValues)
+        returns.append(covariance)
+        frames[asset] = returns
 
     return frames
 
 
-print(getData(["MATICBUSD", "ETHBUSD"], "1w", "1 Jan, 2022", "8 Feb, 2022"))
+
+frames = getData(["MATICBUSD", "ETHBUSD"], "1w", "1 Jan, 2022", "8 Feb, 2022")
+print(frames)
